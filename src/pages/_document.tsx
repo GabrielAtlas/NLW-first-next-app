d3935404ee746d625e755862781c4ac88344c42d
Atlas <blinkplaygm@gmail.com>
2021-02-23 17:47:15 -0300

    XP bar added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="language" content="pt-br" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
